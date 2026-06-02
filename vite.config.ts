1601323a973887c52ed3d43bbc7278865330a9ab
Vinicius Vicentini <viniciusvicentini@live.com>
2026-06-01 22:47:29 -0600

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,24 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
-import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   server: {
     port: 3000,
   },
+  resolve: {
+    tsconfigPaths: true,
+  },
+  build: {
+    rolldownOptions: {
+      checks: {
+        pluginTimings: false,
+        preferBuiltinFeature: false,
+      },
+    },
+  },
   plugins: [
     tailwindcss(),
-    tsConfigPaths({
-      projects: ["./tsconfig.json"],
-    }),
     tanstackStart({
       srcDirectory: "src",
     }),
